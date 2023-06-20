1c2349e0cca28413ac23268f8106b2a6b9aabb2d
Rob Thomson <rob@marotori.com>
2023-06-20 13:11:38 +0100

    Cleanup
    Code cleanup to make more readable.

--- s.portVoltageSensor.ino
+++ s.portVoltageSensor.ino
@@ -1,20 +1,26 @@
 // this is a really simple voltage sensor that is small and light weight for your models.
+// to use it you will need to wire a voltage divider up to the A2 pin on the arduino
+// for my builds I use 22K  and 2K for the divider.  This will allow a working lipo
+// voltage of up to 30v with no issues.  You can of course use different values as long as
+// you drive A2 at lower than 3v.  I have found any higher and the 'dfrobot beetle' boards
+// dont like it.
 
-//voltage divider is:
+// this code relies heavily on this up-stream s.port project
+// https://github.com/RealTadango/FrSky/blob/master/examples/SimpleSensor/SimpleSensor.ino
+// you will need to add his libraries to your arduoino environment for it to work.
 
-///22K
-//2K
+#define SPORT_PIN 9  //digital pin 9
+#define VOLTAGE_PIN A2  //analog pin a2
+#define SENSOR_ID 0x5900  //unique id number for sensor
 
-
-// based on this sort project & manual
-//https://github.com/RealTadango/FrSky/blob/master/examples/SimpleSensor/SimpleSensor.ino
 #include <SPort.h>                  //Include the SPort library
 
-SPortHub hub(0x12, 9);              //Hardware ID 0x12, Software serial pin 3
-SimpleSPortSensor sensor(0x5900);   //Sensor with ID 0x5900
+SPortHub hub(0x12, SPORT_PIN);              //Hardware ID 0x12, Software serial pin 9
+SimpleSPortSensor sensor(SENSOR_ID);   //Sensor with ID 0x5900
+
+
 double kalman_q= 0.05;   
 double kalman_r= 150;   
-
 float dividerRatio = 6.065;
 float sensorValue;
 
@@ -26,15 +32,15 @@ void setup() {
 
 void loop() {
   
-      // read the input on analog pin 0:
-      sensorValue =  kalman_update(analogRead(A2));
+      //simple read of the values from the analog port.. and then send it to the hub.
+      sensorValue =  kalman_update(analogRead(VOLTAGE_PIN));
       float voltage = sensorValue * (5.0 / 1023.0);
-      sensor.value = (voltage * dividerRatio) * 100 ;              //Set the sensor value
-      hub.handle();                     //Handle new data
+      sensor.value = (voltage * dividerRatio) * 100 ;      
+      hub.handle();       
 
 }
 
-
+//kalman filter to 'denoise' the measurements.
 float kalman_update(float measurement)
 {
   //static int lcnt=0;
